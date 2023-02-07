e6e4d1749bae97f944bbcf97f4946f2971ea54d1
Steve Desmond <Steve@ecoAPM.com>
2023-02-07 17:33:53 -0500

    Add Mastodon social media manager

---
+++ TompkinsCOVID.Tests/MastodonTests.cs
@@ -0,0 +1,78 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace TompkinsCOVID.Tests;
+
+public sealed class MastodonTests
+{
+	[Fact]
+	public async Task CanGetLatestDate()
+	{
+		//arrange
+		var response = @"[
+			{ ""content"": ""<p>1/1/2023"" },
+			{ ""content"": ""<p>not an automated post"" },
+			{ ""content"": ""<p>1/2/2023"" }
+		]";
+		var handler = new MockHttpHandler(response);
+		var http = new HttpClient(handler);
+		var settings = new Dictionary<string, string?>
+		{
+			{ "mastodon:instance", "localhost"}
+		};
+		var config = new ConfigurationBuilder().AddInMemoryCollection(settings).Build().GetSection("mastodon");
+		var client = new Mastodon(http, config, string.Empty);
+
+		//act
+		var date = await client.GetLatestPostedDate(string.Empty);
+
+		//assert
+		Assert.Equal(new DateOnly(2023, 1, 2), date);
+	}
+
+	[Fact]
+	public async Task CanPostUpdate()
+	{
+		//arrange
+		var handler = new MockHttpHandler(string.Empty);
+		var http = new HttpClient(handler);
+		var settings = new Dictionary<string, string?>
+		{
+			{ "mastodon:instance", "localhost"}
+		};
+		var config = new ConfigurationBuilder().AddInMemoryCollection(settings).Build().GetSection("mastodon");
+		var client = new Mastodon(http, config, string.Empty);
+
+		//act
+		var record = new Record();
+		await client.Post(record);
+
+		//assert
+		var status = JsonSerializer.Deserialize<JsonElement>(handler.PostBody!).GetProperty("status").GetString();
+		Assert.Equal(record.ToString(), status);
+	}
+
+	private class MockHttpHandler : HttpMessageHandler
+	{
+		private readonly string _response;
+
+		public string? PostBody { get; set; }
+
+		public MockHttpHandler(string response)
+			=> _response = response;
+
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			PostBody = request.Content is not null
+				? await request.Content.ReadAsStringAsync(cancellationToken)
+				: string.Empty;
+
+			return new HttpResponseMessage(HttpStatusCode.OK)
+			{
+				Content = new StringContent(_response)
+			};
+		}
+	}
+}
---
+++ TompkinsCOVID/Mastodon.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace TompkinsCOVID;
+
+public sealed class Mastodon : ISocialMediaManager
+{
+	private readonly HttpClient _http;
+	private readonly IConfigurationSection _config;
+	private readonly string? _accessToken;
+
+	public Mastodon(HttpClient http, IConfigurationSection config, string? accessToken)
+	{
+		_http = http;
+		_config = config;
+		_accessToken = accessToken;
+	}
+
+	public async Task<DateOnly?> GetLatestPostedDate(string username)
+	{
+		var request = new HttpRequestMessage(HttpMethod.Get, $"https://{_config["instance"]}/api/v1/accounts/{_config["id"]}/statuses")
+		{
+			Headers = { Authorization = new AuthenticationHeaderValue("Bearer", _accessToken) }
+		};
+		var response = await _http.SendAsync(request);
+		var json = await JsonSerializer.DeserializeAsync<JsonElement[]>(await response.Content.ReadAsStreamAsync());
+
+		var dates = new List<DateOnly>();
+		foreach (var element in json!)
+		{
+			var text = element.GetProperty("content").GetString();
+			if (DateOnly.TryParse(text?.Split("<p>")[1].Split("<br />")[0], out var date))
+			{
+				dates.Add(date);
+			}
+		}
+
+		return dates.Any() ? dates.Max() : new DateOnly(2022, 11, 16);
+	}
+
+	public async Task Post(Record record)
+	{
+		var content = JsonSerializer.Serialize(record.ToString());
+		var request = new HttpRequestMessage(HttpMethod.Post, $"https://{_config["instance"]}/api/v1/statuses")
+		{
+			Headers = { Authorization = new AuthenticationHeaderValue("Bearer", _accessToken) },
+			Content = new StringContent($"{{ \"status\": {content} }}", new MediaTypeHeaderValue("application/json"))
+		};
+		await _http.SendAsync(request);
+	}
+}
